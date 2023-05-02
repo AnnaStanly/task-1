2e9b408089c980aec1cc2ca96cec30db69554508
AnnaStanly <129248872+AnnaStanly@users.noreply.github.com>
2023-05-02 15:52:19 +0530

    first commit

---
+++ web.js
@@ -0,0 +1,173 @@
+//cnsole.log("hai hello");
+// const=pi
+// var for  variable
+// let....let f=7...then f cannot be reassigned but can change outside the block
+/*{let a=10
+let b=20
+console.log(a+b);}
+var q=7
+var t=3
+console.log(q%t);
+{let e=4
+var t=5
+console.log(e-t);}
+var e=4
+var t=5
+console.log(e*t);
+var e=5
+var t=5
+console.log(e/t);
+//**  AASIGNMENT OPERATORS 
+{let a=5
+let b="5"
+//== is for compare values
+if(a==b)
+console.log("true");
+else
+console.log("false")}
+
+    
+    {let a=5
+        let b="5"
+        //=== is for compare data type
+         if(a===b)
+        console.log("true");
+        else
+        console.log("false")}
+
+
+        {
+
+            let a="anna"
+            let b=" stanly"
+            console.log(a+b);
+            console.log(`${a} and ${b}`)}*/
+        // an object car is created
+        /*let car={
+            model:" calerio",
+            color:"orange",
+            price:12000000
+        }
+
+        console.log(car)
+        console.log(car.price)
+        console.log(car.price + car.model)*/
+
+       /* let person={
+            name:"anna",
+            college:"college",
+            class:"BTech",
+            // as address is multiline use{}
+            address:{
+                place:"thrissur",
+                hname:" home"
+        }}
+        console.log(person)
+        console.log(person.name+person.address.hname)*/
+
+// ARRAY is just a collection of data in js
+
+/*let j=["apple","guova",20,45,6,6]
+console.log(j)
+console.log(j.length)
+j[3]="grapes"
+console.log(j)
+// to add to last element
+    j.push("orange")
+    console.log(j)
+    //  to add as first element
+    j.unshift("banana")
+    console.log(j)
+    // to delete last element 
+    j.pop()
+    console.log(j)
+    // to delete first element
+    j.shift()
+    console.log(j)*/
+
+
+    //ARRAY OF OBJECTS
+
+   /* let v=[
+        {
+            model:"67",
+            price:"340000",
+            name:"nano"
+    },
+     {
+          company:"swish",
+          price:"400000",
+          name:"z23"
+    
+     },
+     {
+
+        father:"hari",
+        mother:"ramya",
+        child:"rahul"
+     }]
+     console.log(v[1].name)
+
+function addTwoNumbers(a,b)
+{
+    console.log(a+b);
+}
+addTwoNumbers(10,20);
+
+add = (a,b) => console.log(a+b);
+add(20,30);
+m = (a,b) => console.log(a*b);
+m(30,40)*/
+
+/*function add(a,b)
+{
+    console.log(a+b);
+    let r= a+b;
+    if(r<10)
+    console.log(r);
+    else
+    console.log("larger");}
+    let a = prompt("enter the first number");
+    let b = prompt("enter the  number");
+add(a+b)*/
+    
+let q=[1,2,3,4,5,6,7,8,9,10];
+    console.log(q);
+    
+printArray=(array) =>{
+    for(let i=0;i<array.length;i++){
+        console.log(array[i]);
+    }
+}
+printArray(q);
+
+while()
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
